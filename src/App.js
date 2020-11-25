3881d72b6c54c91c05ea5f41e431eff1e78a7956
Rafal <orlowskirafa@gmail.com>
2020-11-25 23:00:49 +0100

    fetch data from api simple input/output form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
-
+import SearchBox from './SearchBox';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <p>yo</p>
       </header>
+      <body>
+        <SearchBox/>
+
+      </body>
     </div>
   );
 }
---
+++ src/SearchBox.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './SearchBox.css';
+import SearchIcon from '@material-ui/icons/Search';
+function SearchBox() {
+
+
+    
+    function searchShow(query){
+        fetch(`http://api.tvmaze.com/search/shows?q=${query}`)
+        .then(response =>  response.json())
+        .then((jsonData) =>{
+          const results = jsonData.map(element =>element.show.name);
+         renderResults(results);
+        })
+        .catch(err => {
+          console.error(err);
+        });
+      }
+     let searchTimeoutToken = 0;
+
+      window.onload = () =>{
+          const searchFieldElement = document.getElementById("name");
+          searchFieldElement.onkeyup = (event) =>{
+              clearTimeout(searchTimeoutToken);
+         
+              searchTimeoutToken = setTimeout(() => {
+                searchShow(searchFieldElement.value);
+              },250);
+          }
+      }
+  
+      function renderResults(score){
+        const uldoc = document.getElementById("movie");
+        uldoc.innerHTML = "";
+          score.forEach(result =>{
+            const list = document.createElement("li");
+            list.innerText = result;
+            uldoc.appendChild(list);
+            console.log(result);
+    
+          })
+      }
+    
+
+
+
+
+    return (
+        <div className="searchBox">
+           <form className="InputForm" >
+            <div className="Input2">
+                <label for="name"><SearchIcon/></label>
+                <input type = "text" placeholder="Search" id="name"/>
+                <button>Submit</button>
+            </div>      
+        </form>
+
+        <div className="OutputBox" >
+            <ul id="movie"></ul>
+
+        </div>
+        </div>
+       
+    )
+}
+
+export default SearchBox
